8f7a874501f0143b7aecc32d096de0f2d33367ee
ellpei <ellpei@kth.se>
2018-06-30 18:27:02 +0200

    it's better, worked on make new comment

--- src/components/DetailedView.vue
+++ src/components/DetailedView.vue
@@ -4,9 +4,14 @@
     <h6>{{date}} {{author}}</h6>
     <p>{{body}}</p>
     <DeltaButton postID="id" userID="user()"></DeltaButton>
-    <div id="commentSection" v-for="comment in comments" :key="comment.id">
+    <div id="new-comment">
+      <textarea v-model="newcomment"></textarea>
+      <button type="submit" v-on:click="postComment()">Kommentera</button><br>
+    </div>
+    <div id="comments" v-for="comment in comments" :key="comment.id">
       <h6>{{comment.userID}}</h6>
       <p>{{comment.content}}</p>
+      <DeltaButton postID="comment.id" userID="comment.userID"></DeltaButton>
     </div>
   </div>
 </template>
@@ -20,23 +25,37 @@ export default {
     Comment,
     DeltaButton
   },
+  computed: {
+    user () {
+      return this.$store.state.user
+    }
+  },
+  methods: {
+    postComment: function () {
+      if (this.newcomment.length !== 0) {
+        this.comments.push({userID: this.user, id: '345345', content: this.newcomment})
+        this.newcomment = ''
+        alert(this.newcomment)
+      } else {
+        alert('oops')
+      }
+    }
+  },
   data: function () {
     return {
+      newcomment: '',
       comments: [
         {
           userID: '123456',
+          id: '44444',
           content: 'HejHej kommentar1 blabla'
         },
         {
           userID: '3456',
+          id: '5555',
           content: 'HejHej kommentar2 blabla'
         }
       ]
-    }
-  },
-  computed: {
-    user () {
-      return this.$store.state.user
     }
   }
 }
@@ -44,16 +63,19 @@ export default {
 <style scoped lang="scss">
 @import '@/styles/color.scss';
 @import '@/styles/layout.scss';
+@import '@/styles/input.scss';
 
 #detailed {
   margin: 20px;
   padding-left: 10px;
 }
 
-#commentSection {
-  background-color: red;
-  border-radius: 15px;
+#comments {
+  border-radius: 5px;
+  border-style: solid;
+  border-color: $warm-grey;
   padding: 10px;
+  margin-top: 5px;
   margin-bottom: 5px;
 }
 </style>
