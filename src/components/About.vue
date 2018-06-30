904479cecd7a02235976055f29ea4c73517da03f
ellpei <ellpei@kth.se>
2018-06-30 18:43:20 +0200

    fixed jonas fel

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="container" align="center">
     <div class="about"> Om oss </div>
-    <div class="about-text"> 
-      Delta är ett initiativ att öka tillgängligheten på Svensk riksdags information. 
+    <div class="about-text">
+      Delta är ett initiativ att öka tillgängligheten på Svensk riksdags information.
       Genom att förenkla disskution kring motioner och dokument hoppas vi kunna stärka
       det Svenska politiska deltagandet.
-    </div> 
-
+    </div>
     <div class="contact">
       <div class="contact-field">
-        <div class="contact-icon"> <img src="@/assets/vintage-telephone-call.svg" width="30px" height="30px" alt="Telephone missing."> </img></div>
+        <div class="contact-icon"> <img src="@/assets/vintage-telephone-call.svg" width="30px" height="30px" alt="Telephone missing."></div>
         <div class="contact-cell"> Telefon: 070 133 13 39</div>
       </div>
       <div class="contact-field">
-        <div class="contact-icon"> <img src="@/assets/envelope.svg" width="30px" height="30px" alt="Email missing."> </img> </div>
+        <div class="contact-icon"> <img src="@/assets/envelope.svg" width="30px" height="30px" alt="Email missing."></div>
         <div class="contact-cell"> Email: delta.snälla@demokrati.se </div>
       </div>
     </div>
@@ -27,7 +26,6 @@ export default {
 <style scoped lang="scss">
 @import '@/styles/mixins/positioning.scss';
 @import '@/styles/color.scss';
-
 
 .contact, .contact-field {
   display: flex;
